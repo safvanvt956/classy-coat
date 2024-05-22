52e44beccd0489c4ebcaa4aa77eb419338b3d3b7
Safvan <safvanvt956@gmail.com>
2024-05-22 17:33:51 +0530

    up

--- js/scripts.js
+++ js/scripts.js
@@ -74,41 +74,77 @@ showSlide(slideIndex);
 
 
 
-var swiper = new Swiper(".swiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    coverflowEffect: {
-      rotate: 0,
-      stretch: 0,
-      depth: 100,
-      modifier: 4,
-      slideShadows: true
-    },
-    loop: true,
-    // Navigation arrows
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev"
-    },
-    keyboard: {
-      enabled: true
-    },
-    mousewheel: {
-      thresholdDelta: 70
-    },
-    breakpoints: {
-      560: {
-        slidesPerView: 2.5
-      },
-      768: {
-        slidesPerView: 3
-      },
-      1024: {
-        slidesPerView: 3
-      }
-    }
-  });
+// var swiper = new Swiper(".swiper", {
+//     effect: "coverflow",
+//     grabCursor: true,
+//     centeredSlides: true,
+//     coverflowEffect: {
+//       rotate: 0,
+//       stretch: 0,
+//       depth: 100,
+//       modifier: 4,
+//       slideShadows: true
+//     },
+//     loop: true,
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev"
+//     },
+//     keyboard: {
+//       enabled: true
+//     },
+//     mousewheel: {
+//       thresholdDelta: 70
+//     },
+//     breakpoints: {
+//       560: {
+//         slidesPerView: 2.5
+//       },
+//       768: {
+//         slidesPerView: 3
+//       },
+//       1024: {
+//         slidesPerView: 3
+//       }
+//     }
+//   });
 
+
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: false,
+  centeredSlides: true,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 4,
+    slideShadows: true
+  },
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".prev",
+    prevEl: ".next"
+  },
+  keyboard: {
+    enabled: true
+  },
+  // mousewheel: {
+  //   thresholdDelta: 70
+  // },
+  breakpoints: {
+    560: {
+      slidesPerView: 2.5
+    },
+    768: {
+      slidesPerView: 3
+    },
+    1024: {
+      slidesPerView: 3
+    }
+  }
+});
 
   
